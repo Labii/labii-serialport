9315c0525cbea6a9bcb50f679876b3785fd924c6
Yonggan Wu <weasteam@users.noreply.github.com>
2023-01-28 01:49:40 -0800

    support write serial

--- main.ts
+++ main.ts
@@ -8,8 +8,17 @@ input.onButtonPressed(Button.B, function () {
     serial.writeString("LABII")
     basic.showIcon(IconNames.Yes)
 })
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    basic.showString(serial.readUntil(serial.delimiters(Delimiters.NewLine)))
+    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+})
+basic.showIcon(IconNames.Heart)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 serial.redirect(
 SerialPin.USB_TX,
 SerialPin.USB_RX,
 BaudRate.BaudRate9600
 )
+basic.forever(function () {
+	
+})

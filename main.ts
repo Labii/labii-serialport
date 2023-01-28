f3a66096d1a50b0b8ad32bf3d5e25aa461721616
Yonggan Wu <weasteam@users.noreply.github.com>
2023-01-27 22:00:29 -0800

    update the functions

--- main.ts
+++ main.ts
@@ -1,23 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     music.playTone(988, music.beat(BeatFraction.Whole))
     serial.writeNumber(input.temperature())
-    serial.writeLine("")
     basic.showNumber(input.temperature())
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
     serial.writeString("LABII")
-    serial.writeLine("")
-    basic.showString("LABII")
+    basic.showIcon(IconNames.Yes)
 })
 serial.redirect(
 SerialPin.USB_TX,
 SerialPin.USB_RX,
 BaudRate.BaudRate9600
 )
-basic.forever(function () {
-	
-})
